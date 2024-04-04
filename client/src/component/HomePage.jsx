97df5f058e6bb10f64e5b5d0bddbb28c42ee2653
s7659 <nb295114@gmail.com>
2024-04-04 17:26:40 +0300

    update homePage

--- client/src/component/HomePage.jsx
+++ client/src/component/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage=()=>{
   return( <>
   <body style={{textAlign:'center', backgroundColor:'#FEEBD5'}}>
-    <h1 style={{fontFamily:'Comic Sans MS'}}>OUR FAMILY ON SHOBBOS</h1>
+    <h1 style={{fontFamily:'Comic Sans MS'}}>OUR FAMILY ON SHOBBOS!!!!!!!</h1>
     <img src="./שבת.png" ></img>
   </body>
 
